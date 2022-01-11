aac4ad289462cd2e019cb74e264768dd715f0e49
Ankit Sharma <39131599+Mistaken-JOKESTER@users.noreply.github.com>
2022-01-11 10:45:11 +0530

    update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBLI5c9vjSzKyKw0ggLAyceHBnq7SLCscw",
-    authDomain: "firestrom-9a412.firebaseapp.com",
-    projectId: "firestrom-9a412",
-    storageBucket: "firestrom-9a412.appspot.com",
-    messagingSenderId: "96506211442",
-    appId: "1:96506211442:web:21958418ad6f6d2abfa6eb"
+    apiKey: //API KEY,
+    authDomain: //authDomain,
+    projectId: //ProjectID,
+    storageBucket: //storageBucket,
+    messagingSenderId: //messaging sender id,
+    appId: //appid
 }
   
 // Initialize Firebase
